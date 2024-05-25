d02c30e1edec77ce247a6e71a718e5e98f434ca6
Rohit <rohit.verma@itsrizzoli.it>
2024-05-25 23:15:00 +0200

    Updated homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import {FlatList, Text, View} from "react-native";
 import style from "@/styles/Styles";
 import {Link, Stack} from "expo-router";
+import {AntDesign} from '@expo/vector-icons';
 
-const polls = [{id: 1},{id: 2},{id: 3}]
+const polls = [{id: 1}, {id: 2}, {id: 3}]
 export default function HomeScreen() {
     return (
         <>
@@ -11,15 +12,20 @@ export default function HomeScreen() {
                 headerStyle: {backgroundColor: "#1e1c1c"},
                 headerTintColor: "white",
                 headerTitleAlign: "center",
+                headerRight: (() => (
+                    <Link href={"/poll/new"} style={{margin: 8}}>
+                        <AntDesign name="pluscircleo" size={24} color="white"/>
+                    </Link>
+                )),
             }}/>
             <View
                 style={style.mainContainer}
             >
-                <Text style={style.title}>Hello</Text>
+                <Text style={style.title}>Poll Free</Text>
                 <FlatList
                     contentContainerStyle={{gap: 8}}
                     data={polls} renderItem={(poll) => (
-                    <Link style={style.pollContainer} href={"/hooks/"+poll.item.id}>
+                    <Link style={style.pollsContainer} href={"/poll/" + poll.item.id}>
                         <Text>{poll.item.id}. Yes or No</Text>
                     </Link>
 
