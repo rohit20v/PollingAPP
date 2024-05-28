6455aff293e32cc3b203b686eecf3e8d9ca2dc46
Rohit <rohit.verma@itsrizzoli.it>
2024-05-28 22:06:32 +0200

    Updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import {Stack} from "expo-router";
+import AuthProvider from "@/app/providers/AuthProvider";
 
 export default function RootLayout() {
     return (
-        <Stack />
+        <AuthProvider>
+            <Stack>
+                <Stack.Screen name={"(auth)"} options={{
+                    title: "User Authentication",
+                    headerTintColor: "white",
+                    headerTitleAlign: "center",
+                    headerStyle: {
+                        backgroundColor: "#202020"
+                    }
+                }}/>
+            </Stack>
+        </AuthProvider>
     );
 }
